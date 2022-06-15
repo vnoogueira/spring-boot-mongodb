01c9edeee083c878404fbd25fb2be1c4c435d51b
Vitor Ferreira <vitor.nogueira94@gmail.com>
2022-06-15 20:52:26 -0300

    Instanciação da base de dados

---
+++ src/main/java/com/vitorferreira/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.vitorferreira.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.vitorferreira.springbootmongodb.domain.User;
+import com.vitorferreira.springbootmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User jorge = new User(null, "Jorge Blue", "jorge@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, jorge));
+
+	}
+
+}
