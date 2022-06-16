2482fa383d165475662cd0dfe8ad348fa8354939
Vitor Ferreira <vitor.nogueira94@gmail.com>
2022-06-15 21:08:28 -0300

    Implementing UserDTO

--- src/main/java/com/vitorferreira/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/vitorferreira/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.vitorferreira.springbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitorferreira.springbootmongodb.domain.User;
+import com.vitorferreira.springbootmongodb.dto.UserDto;
 import com.vitorferreira.springbootmongodb.services.UserService;
 
 
@@ -20,9 +22,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)// Metodo Get
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDto>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); // Retornando os usuarios do list com status http 200-OK
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto); // Retornando os usuarios do list com status http 200-OK
 	}
 	
 }
