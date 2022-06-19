f89770c90e36659a82aba8799a67966658e9054d
Vitor Ferreira <vitor.nogueira94@gmail.com>
2022-06-19 19:20:10 -0300

    Insert User with POST

--- src/main/java/com/vitorferreira/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/vitorferreira/springbootmongodb/resources/UserResource.java
@@ -1,19 +1,21 @@
 package com.vitorferreira.springbootmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitorferreira.springbootmongodb.domain.User;
 import com.vitorferreira.springbootmongodb.dto.UserDto;
 import com.vitorferreira.springbootmongodb.services.UserService;
-
 
 @RestController // Metoto do Spring para informar que é um metodo REST
 @RequestMapping(value = "/users") // Identificando que será chamado pela url users
@@ -22,18 +24,25 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)// Metodo Get
+	@RequestMapping(method = RequestMethod.GET) // Metodo Get
 	public ResponseEntity<List<UserDto>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto); // Retornando os usuarios do list com status http 200-OK
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
- 	public ResponseEntity<UserDto> findById(@PathVariable String id) {
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDto(obj));
 	}
 
-	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDto objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/vitorferreira/springbootmongodb/services/UserService.java
+++ src/main/java/com/vitorferreira/springbootmongodb/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitorferreira.springbootmongodb.domain.User;
+import com.vitorferreira.springbootmongodb.dto.UserDto;
 import com.vitorferreira.springbootmongodb.repository.UserRepository;
 import com.vitorferreira.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
