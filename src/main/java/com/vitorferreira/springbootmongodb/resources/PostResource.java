30327e24932ea78d4a57a6ea84fa28576a797332
Vitor Ferreira <vitor.nogueira94@gmail.com>
2022-06-22 20:26:00 -0300

    Implemented method findAll() of posts

---
+++ src/main/java/com/vitorferreira/springbootmongodb/resources/PostResource.java
@@ -0,0 +1,35 @@
+package com.vitorferreira.springbootmongodb.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vitorferreira.springbootmongodb.domain.Post;
+import com.vitorferreira.springbootmongodb.services.PostService;
+
+@RestController // Metoto do Spring para informar que é um metodo REST
+@RequestMapping(value = "/posts") // Identificando que será chamado pela url users
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> posts = service.findAll();
+		return ResponseEntity.ok().body(posts);
+
+	}
+
+}
---
+++ src/main/java/com/vitorferreira/springbootmongodb/services/PostService.java
@@ -0,0 +1,29 @@
+package com.vitorferreira.springbootmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitorferreira.springbootmongodb.domain.Post;
+import com.vitorferreira.springbootmongodb.repository.PostRepository;
+import com.vitorferreira.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+		
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
