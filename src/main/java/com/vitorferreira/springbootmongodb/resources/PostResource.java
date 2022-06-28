36426b013cf0c9677b883c8a4108c4cfe952d080
Vitor Ferreira <vitor.nogueira94@gmail.com>
2022-06-27 22:15:04 -0300

    Consulta com varios criterios

--- src/main/java/com/vitorferreira/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/vitorferreira/springbootmongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.vitorferreira.springbootmongodb.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,13 +34,25 @@ public class PostResource {
 		return ResponseEntity.ok().body(posts);
 
 	}
-	
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URI.DecodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
-		
+
+	}
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URI.DecodeParam(text);
+		Date min = URI.convertDate(text, new Date(1L));
+		Date max = URI.convertDate(text, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+
 	}
 
 }
--- src/main/java/com/vitorferreira/springbootmongodb/services/PostService.java
+++ src/main/java/com/vitorferreira/springbootmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.vitorferreira.springbootmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +29,11 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
